1c67432375d0447886e57eaa698059ace628ceff
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-29 20:56:00 +0800

    implement logic for submenus

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import sublinks from '../data';
+import { useGlobalContext } from '../utils/context';
 
 const NavLinks = () => {
+  const { setPageId } = useGlobalContext();
   return (
     <div className="nav-links">
       {sublinks.map((item) => {
         const { pageId, page } = item;
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            onMouseEnter={() => setPageId(pageId)}
+            key={pageId}
+            className="nav-link"
+          >
             {page}
           </button>
         );
